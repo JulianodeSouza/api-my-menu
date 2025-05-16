1236b61f6eb5a577c1fb99243a7a5f0cd0133a65
JulianodeSouzagit config --global user.name JulianodeSouzagit config --global user.name JulianodeSouza <julianorafaelds878@gmail.com>
2025-05-16 18:36:02 -0300

    fix: change route

--- src/api/routes/supermarket/supermaket.ts
+++ src/api/routes/supermarket/supermaket.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
 
 export async function getSupermarketList(_req: Request, _res: Response) {
-  throw new Error("Rota não implementada");
+  _res.json({});
 }
